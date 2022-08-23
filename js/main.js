4e51a868b76f4ee9f6590765dd200b20d6f29a50
rivas.will <rivas.will.1000@gmail.com>
2022-08-23 08:20:12 -0300

    update

---
+++ js/main.js
@@ -0,0 +1,7 @@
+const mail = document.querySelector('#loginName');
+const psswd = document.querySelector('#loginPassword');
+const boton = document.querySelector('#signIn');
+
+boton.addEventListener('click', ()=>{
+    window.location.href='./index.html'
+})
